59d1cd344d3803ac7c9a864ea5d775533d348318
Carlo Abalos <carloabalos24@gmail.com>
2018-03-01 09:49:31 +0800

    Express Generator REST API

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
 
-var app = express();
+var app = express();dishRouter
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
