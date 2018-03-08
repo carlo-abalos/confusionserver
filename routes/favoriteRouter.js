993f14346e791799cbddc945072ab8e93cbb35ad
Carlo Abalos <carloabalos24@gmail.com>
2018-03-08 12:14:55 +0800

    Integrating the Client and Server: Updating the REST Server

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -98,8 +98,27 @@ favoriteRouter.route('/')
 favoriteRouter.route('/:dishId')
 .options(cors.corsWithOptions, (req, res) => {res.sendStatus(200);})
 .get(cors.cors, (req,res,next) => {
-	res.statusCode = 403;
-	res.end("GET operation not supported on /favorites/:dishId");
+	Favorites.findOne({user: req.user._id})
+	.then((favorites) => {
+		if(!favorites){
+			res.statusCode = 200;
+			res.setHeader("Content-Type", "application/json");
+			return res.json({"exists": false, "favorites": favorites});
+		}
+		else{
+			if(favorites.dishes.indexOf(req.params.dishId) < 0){
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "application/json");
+				return res.json({"exists": false, "favorites": favorites});
+			}
+			else{
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "application/json");
+				return res.json({"exists": true, "favorites": favorites});
+			}	
+		}
+	}, (err) => next(err))
+	.catch((err) => next(err));
 })
 .post(cors.corsWithOptions, authenticate.verifyUser, 
 	(req, res, next) => {
@@ -164,7 +183,12 @@ favoriteRouter.route('/:dishId')
 
 				favorite.save()
 				.then((favorite) => {
-					res.json(favorite);	
+					Favorites.findById(favorite._id)
+					.populate('user')
+					.populate('dishes')
+					.then((favorite) => {
+						res.json(favorite);
+					});	
 				}, (err) => next(err));
 			}
 			else{
